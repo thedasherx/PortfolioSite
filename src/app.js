afc462c7c83e4402aba371d29b3e0a2993c6a45f
thedasherx <75217270+rhit-webbma@users.noreply.github.com>
2022-06-28 20:09:13 -0400

    Material UI imports

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import React from "react";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
